fe2a184fcbdb5f44252a955aba174f71c0adc0dc
陈炳 <32173777@qq.com>
2019-02-21 11:53:12 +0800

    fine-tuning

--- two_sum/two_sum.go
+++ two_sum/two_sum.go
@@ -4,8 +4,8 @@ func twoSum(nums []int, target int) []int {
 	m := make(map[int]int)
 	for i := 0; i < len(nums); i++ {
 		another := target - nums[i]
-		if _, ok := m[another]; ok {
-			return []int{m[another], i}
+		if v, ok := m[another]; ok {
+			return []int{v, i}
 		}
 		m[nums[i]] = i
 	}
